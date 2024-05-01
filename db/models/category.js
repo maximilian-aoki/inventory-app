90456588fb2fdd9a10e17b19ed0dff6163717d06
Maximilian Aoki <135271252+maximilian-aoki@users.noreply.github.com>
2024-04-30 22:42:59 -0400

    add category and item schemas with virtuals for url

--- db/models/category.js
+++ db/models/category.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({});
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 25,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+  },
+  imgUrl: {
+    type: String,
+    minLength: 2,
+  },
+});
+
+categorySchema.virtual("url").get(function () {
+  return `/inventory/categories/${this._id}`;
+});
 
 module.exports = mongoose.model("Category", categorySchema);
--- db/models/item.js
+++ db/models/item.js
@@ -1,5 +1,41 @@
 const mongoose = require("mongoose");
 
-const itemSchema = new mongoose.Schema({});
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 25,
+  },
+  description: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 100,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  numberInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 100,
+  },
+  imgUrl: {
+    type: String,
+    minLength: 2,
+  },
+});
+
+itemSchema.virtual("url").get(function () {
+  return `/inventory/items/${this._id}`;
+});
 
 module.exports = mongoose.model("Item", itemSchema);
